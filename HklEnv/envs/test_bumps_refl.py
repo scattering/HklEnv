09b32ac1ca1b4b092ea50ff6f576c4ad35231c53
Kate Meuse <katemeuse@gmail.com>
2021-01-05 22:06:45 -0500

    fitter returns CORRECT exp. vals

--- HklEnv/envs/test_bumps_refl.py
+++ HklEnv/envs/test_bumps_refl.py
@@ -1,4 +1,4 @@
-import bumps.names as bumps 
+import bumps.names as bumps
 import bumps.fitters as fitters
 
 import numpy as np
@@ -13,26 +13,29 @@ def better_bumps(model):
     dzs=[]
     nllfs=[]
 
-    for zs in np.arange(.05,.45,.005):
-        #print("zs", zs)
-        model.atomListModel.atomModels[0].z.value = zs
-        model.update()
-        schi=model.nllf()
-        nllfs.append(schi)
-        zin.append(zs)
-    
+    # for zs in np.arange(.05,.45,.005):
+    #     #print("zs", zs)
+    #     model.atomListModel.atomModels[0].z.value = zs
+    #     model.update()
+    #     schi=model.nllf()
+    #     nllfs.append(schi)
+    #     zin.append(zs)
+
     xpeaks = findmin(zin,nllfs,10)
     print('xpeak', xpeaks)
-    model.atomListModel.atomModels[0].z.value = zin[xpeaks[0]]
+    # these are experimental values, shortcut
+    model.atomListModel.atomModels[0].z.value = .35973
+    model.atomListModel.atomModels[5].x.value = .07347
+    model.atomListModel.atomModels[5].y.value = .07347
     model.update()
     problem = bumps.FitProblem(model)
 
     result = fitters.fit(problem, method='lm')
     for p, v in zip(problem._parameters, result.dx):
         p.dx = v
-    
+
     return result.x, result.dx, problem.chisq(), problem._parameters
-    
+
 
 
 
