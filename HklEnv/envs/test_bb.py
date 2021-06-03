aa9a9ff170ca1a9a4ea3b24311a9c78475b2af25
Kate Meuse <katemeuse@gmail.com>
2021-06-03 13:10:48 -0400

    add chisquared surface generation

--- HklEnv/envs/test_bb.py
+++ HklEnv/envs/test_bb.py
@@ -6,15 +6,14 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 sys.path.append("/home/kaet/gen/nist/pycrysfml")
-print(sys.path)
 
 from hklgen import fswig_hklgen as H
 from hklgen import hkl_model as Mod
 from hklgen import sxtal_model as S
 
-from test_bumps_refl import better_bumps
+from test_bumps_refl import better_bumps, graph
 
-def step(action):
+def step(actions):
     visited.append(refList[int(actions)])
 
     model.refList = H.ReflectionList(visited)
@@ -26,51 +25,69 @@ def step(action):
     observed.append(sfs2[int(actions)])
     model._set_observations(observed)
     model.update()
+    print(actions)
+    if (len(visited) > 3):
+        x, dx, chisq, params = better_bumps(model)
+        print("dx:", dx)
 
-    print(better_bumps(model))
+# TODO: unhardcode if sharing code
+observedFile = "/home/kaet/gen/nist/pycrysfml/hklgen/examples/sxtal/prnio.int"
+infoFile = "/home/kaet/gen/nist/pycrysfml/hklgen/examples/sxtal/prnio.cfl"
 
-if __name__ == "main":
-    # TODO: unhardcode if sharing code
-    observedFile = "/home/kaet/gen/nist/pycrysfml/hklgen/examples/sxtal/prnio.int"
-    infoFile = "/home/kaet/gen/nist/pycrysfml/hklgen/examples/sxtal/prnio.cfl"
+# Read data
+spaceGroup, crystalCell, atomList = H.readInfo(infoFile)
 
-    # Read data
-    spaceGroup, crystalCell, atomList = H.readInfo(infoFile)
+# Return wavelength, refList, sfs2, error, two-theta, and four-circle parameters
+wavelength, refList, sfs2, error = S.readIntFile(observedFile, kind="int", cell=crystalCell)
+wavelength = wavelength
+refList = np.array(refList)
+sfs2 = sfs2
+error = error
+tt = [H.twoTheta(H.calcS(crystalCell, ref.hkl), wavelength) for ref in refList]
+backg = None
+exclusions = []
+visited = []
+observed = []
 
-    # Return wavelength, refList, sfs2, error, two-theta, and four-circle parameters
-    wavelength, refList, sfs2, error = S.readIntFile(observedFile, kind="int", cell=crystalCell)
-    print(error)
-    wavelength = wavelength
-    refList = np.array(refList)
-    sfs2 = sfs2
-    error = error
-    tt = [H.twoTheta(H.calcS(crystalCell, ref.hkl), wavelength) for ref in refList]
-    backg = None
-    exclusions = []
-    visited = []
-    observed = []
+# from reset()
+cell = Mod.makeCell(crystalCell, spaceGroup.xtalSystem)
 
-    # from reset()
-    cell = Mod.makeCell(crystalCell, spaceGroup.xtalSystem)
+# Define a model
+model = S.Model([], [], backg, wavelength, spaceGroup, cell,
+                     atomList, exclusions,
+                     scale=0.06298, error=[], extinction=[0.0001054])
 
-    # Define a model
-    model = S.Model([], [], backg, wavelength, spaceGroup, cell,
-                         atomList, exclusions,
-                         scale=0.06298, error=[], extinction=[0.0001054])
+# Set a range on the x value of the first atom in the model
+# model.atomListModel.atomModels[0].z.value = 0.25
+# model.atomListModel.atomModels[0].z.range(0, 0.45)
 
-    # Set a range on the x value of the first atom in the model
-    model.atomListModel.atomModels[0].z.value = 0.25
-    model.atomListModel.atomModels[0].z.range(0, 0.45)
+model.atomListModel.atomModels[5].x.value = 0.1
+model.atomListModel.atomModels[5].x.range(0, 0.2)
 
-    model.atomListModel.atomModels[5].x.value = 0.1
-    model.atomListModel.atomModels[5].x.range(0, 0.2)
+model.atomListModel.atomModels[5].y.value = 0.1
+model.atomListModel.atomModels[5].y.range(0, 0.2)
 
-    model.atomListModel.atomModels[5].y.value = 0.1
-    model.atomListModel.atomModels[5].y.range(0, 0.2)
+model.update()
 
-    model.update()
+actions = np.zeros(198)
 
-    actions = np.zeros(198)
 
-    for i in range(8):
-        step(np.random.randint(0, 198)) # randomly pick action
+for i in range(20):
+    step(np.random.randint(0, 198)) # randomly pick action
+    # step(i)
+
+
+# step(83)
+# step(88)
+# step(148)
+# step(18)
+
+# step(145)
+# step(19)
+# step(105)
+# step(183)
+# step(109)
+# step(57)
+
+graph(model)
+
--- HklEnv/envs/test_bumps_refl.py
+++ HklEnv/envs/test_bumps_refl.py
@@ -1,25 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 import bumps.names as bumps
 import bumps.fitters as fitters
 
+
 def better_bumps(model):
-
-    zin=[]
-    zout=[]
-    chis=[]
-    dzs=[]
-    nllfs=[]
-
-    # for zs in np.arange(.05,.45,.005):
-    #     #print("zs", zs)
-    #     model.atomListModel.atomModels[0].z.value = zs
-    #     model.update()
-    #     schi=model.nllf()
-    #     nllfs.append(schi)
-    #     zin.append(zs)
-
-    # xpeaks = findmin(zin,nllfs,10)
-    # print('xpeak', xpeaks)
-    # these are experimental values, shortcut
     model.atomListModel.atomModels[0].z.value = .35973
     model.atomListModel.atomModels[5].x.value = .07347
     model.atomListModel.atomModels[5].y.value = .07347
@@ -33,8 +19,32 @@ def better_bumps(model):
     return result.x, result.dx, problem.chisq(), problem._parameters
 
 
+def chi_surface(model):
+    xin = []
+    chis = []
 
+    for xs in np.arange(.05, .45, .005):
+        model.atomListModel.atomModels[5].y.value = xs
+        model.update()
+        xin.append(xs)
+        try:
+            problem = bumps.FitProblem(model)
+        except:
+            print("ERROR AT:" + xs)
 
+        result = fitters.fit(problem, method='lm')
+        for p, v in zip(problem._parameters, result.dx):
+            p.dx = v
 
+        chis.append(problem.chisq())
 
+    return xin, chis
 
+def graph(model):
+    xin, chis = chi_surface(model)
+    plt.plot(xin, chis, 'bo')
+    plt.axvline(x=0.07347)
+    plt.ylabel("chi squared")
+    plt.xlabel("Oy estimated value")
+    plt.savefig("/home/kaet/gen/nist/HklEnv/HklEnv/envs/test.png")
+    plt.close()
